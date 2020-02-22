4ef14467fba3d1dd372f3cf4a44855685dcc06a6
kaitlynning <kittyning88308@gmail.com>
2020-02-22 14:51:36 +0800

    my commit

--- k.py
+++ k.py
@@ -1,11 +1,13 @@
 first_name = "Kaitlyn"
 last_name = "Ning"
+
 print("Hi, my name is " + first_name + " " + last_name + ".")
-print(f"I'm {20 + 6 } years old.")
+print(f"I'm {20 + 6} years old.")
 print("Imma pretty girl.")
 """
+
 Hi, my name is Kaitlyn Ning.
 I'm 26 years old.
 Imma pretty girl.
-...
+
 """
