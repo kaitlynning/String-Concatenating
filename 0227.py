58de8a0d508b97adb3c619cc560a9c6de8cf96ce
kaitlynning <kittyning88308@gmail.com>
2020-02-27 22:20:27 +0800

    comprehension practice

---
+++ 0227.py
@@ -0,0 +1,17 @@
+K_letters = []
+
+for letter in 'Kaitlyn':
+	K_letters.append(letter)
+#use for and append
+
+print(K_letters)
+
+K_letters = [letter for letter in 'Kaitlyn']
+#use list comprehension
+
+print(K_letters)
+
+'''
+['K', 'a', 'i', 't', 'l', 'y', 'n']
+['K', 'a', 'i', 't', 'l', 'y', 'n']
+'''
