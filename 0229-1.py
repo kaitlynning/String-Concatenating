c2ba5c27c41c98cd436b7dd512bd102e5ac24a9f
kaitlynning <kittyning88308@gmail.com>
2020-02-29 16:30:45 +0800

    reverse

---
+++ 0229-1.py
@@ -0,0 +1,11 @@
+num = int(input('num = '))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    num //= 10
+print(reversed_num)
+
+'''
+num = 123456789123456789
+987654321987654321
+'''
